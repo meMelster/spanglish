daa7c780947848bd931511ae53250d7eafbbe5e8
meMelster <mserra17@fau.edu>
2017-01-04 01:48:24 +0000

    Spanglish tweet data.

--- spanglish_tweets.py
+++ spanglish_tweets.py
@@ -7,13 +7,15 @@ import json
 
 
 #spanglishRegex = r'(#|@)*(S|s)panglish*'
-#spanglish_tweets_file = open('spanglish_tweets.tsv', 'w+')
+spanglish_tweets_file = open('data/spanglish_tweets.txt', 'w')
 
 english_dictionary = enchant.Dict("en_US")
 spanish_dictionary = enchant.Dict("es_US")
 
 english_word = False
 spanish_word = False
+
+spanglish_tweets = []
 
 emoticons_str = r"""
     (?:
@@ -55,21 +57,35 @@ line_generator = open(sys.argv[1])
 for line in line_generator:
     line_object = json.loads(line)
 
-    tweet = line_object["text"]
-    tweet_words = preprocess(tweet)
+    if "text" in line_object:
+        tweet = line_object["text"]
+        tweet_words = preprocess(tweet)
 
-    # if re.search(r'spanglishRegex', tweet) != None:
-    #     spanglish_tweets_file.write(*fields, sep='\t')
-    #     spanglish_tweets_file.write('\n')
+        # if re.search(r'spanglishRegex', tweet) != None:
+        #     spanglish_tweets_file.write(*fields, sep='\t')
+        #     spanglish_tweets_file.write('\n')
 
-    # #we should check for Spanglish words as well
+        # #we should check for Spanglish words as well
 
-    for word in tweet_words:
-        if english_dictionary.check(word):
-            english_word = True
-            print("English Word!  " + word)
-        if spanish_dictionary.check(word):
-            spanish_word = True
-            print("Spanish Word!  " + word)
-    if english_word and spanish_word:
-        print("Spanglish Tweet!  " + tweet)
+        for word in tweet_words:
+            if english_dictionary.check(word):
+                english_word = True
+                spanglish_tweets_file.write("English Word!  " + word + '\n')
+            if spanish_dictionary.check(word):
+                spanish_word = True
+                spanglish_tweets_file.write("Spanish Word!  " + word + '\n')
+        if english_word and spanish_word:
+            #print("Spanglish Tweet!  " + tweet)
+            spanglish_tweets_file.write(tweet + '\n')
+
+spanglish_tweets_file.close()
+
+
+
+#TF-IDF feature extraction with scikit-learn
+# vectorizer = TfidfVectorizer(min_df=5,
+#                              max_df = 0.8,
+#                              sublinear_tf=True,
+#                              use_idf=True)
+# train_vectors = vectorizer.fit_transform(train_data)
+# test_vectors = vectorizer.transform(train_data)
